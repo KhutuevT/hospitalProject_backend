bcee54b103bbf1d9139d21fd34c3b3db1244ca95
Tamerlan Khutuev <t.khutuev@gmail.com>
2022-01-12 16:27:07 +0300

    add validation

--- src/modules/controllers/user.controller.js
+++ src/modules/controllers/user.controller.js
@@ -11,18 +11,23 @@ class UserController {
   async registration(req, res, next) {
     try {
       const { login, password } = req.body;
+      const loginIsValid = login.match(/^[а-яА-Яa-zA-Z\d]{6,}$/gm);
+      const passwordIsValid = password.match(/^(?=.*\d)[a-zA-Z\d]{6,}$/gm);
+      if (!loginIsValid || !passwordIsValid) {
+        return res.status(400).json({ message: "Invalid password or login" });
+      }
       const candidate = await User.findOne({ login });
       if (candidate) {
         return res
           .status(400)
-          .json({ message: "Пользователь с таким именем уже существует." });
+          .json({ message: "A user with this login already exists." });
       }
 
       const hashPassword = await bcrypt.hash(password, 3);
       const user = new User({ login, password: hashPassword });
       await user.save();
       const token = generateAccessToken(user._id);
-      return res.json({token});
+      return res.json({ token });
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: "Registration error" });
